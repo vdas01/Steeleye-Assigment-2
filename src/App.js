1d646707d402a67921f38a0a49ae31549ef1e7d8
Vishal Das <vdas53073@gmail.com>
2023-08-06 13:47:30 +0530

    added minor changes

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ function App() {
   const plainText = "Hi David Headline: Energix Closes $520 Million Financing and Tax Equity Deal to Fund New Solar Projects Summary: Two deals with Morgan Stanley Renewables Inc. and Santander CIB will help finance the construction and operation of six utility Equity scale solar… Read the full article here ------------------------------------- You received this because you are subscribed to news related to ES0113900J37, and this story was marked as 82% relevant. Copyright of PR Newswire. All Rights Reserved. Terms and Conditions | Privacy Policy. To stop PR Newswire emails from getting removed by email filters please add our address (noreply@prnewswire.com) to your email address book. Registered Office: 3 Spring Mews, London SE11 5AN. Tel: +44 (0) 207 8405100. To unsubscribe change your email preferences, please click here -------------------------------------";
 
   const plainTextPositions = [
-    { start: 20, end: 30 },
-    { start: 3, end: 7 },
+    { start: 1, end: 2 },
+    { start: 5, end: 7 },
     { start: 9, end: 15 }
   ];
   return (
--- src/func.jsx
+++ src/func.jsx
@@ -7,9 +7,10 @@
     plainTextPositions.forEach(pos => {
       const word = plainText.slice(pos.start, pos.end + 1);
       const startId = htmlContent.indexOf(word, currPos);
+      const endId = startId + word.length;
       ans.push(htmlContent.slice(currPos, startId));
       ans.push('<mark>' + word + '</mark>');
-      currPos = pos.end + 1;
+      currPos = endId;
     });
 
     ans.push(htmlContent.slice(currPos));
